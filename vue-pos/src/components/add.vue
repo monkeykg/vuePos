227a897373cec6495ad4fabff939b866b65d3231
monkeykg <923192905@qq.com>
2018-04-19 21:40:58 +0800

    test

---
+++ vue-pos/src/components/add.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="add">
+      <ul>
+          <li v-for="(val,index) in demo">
+              <button @click="reduce(index)">-</button>
+              <span>{{val}}</span>
+              <button @click="addnum(index)">+</button>
+          </li>
+      </ul>
+  </div>
+</template>
+<script>
+import Vue from 'vue'
+export default {
+  name:'add',
+  data(){
+      return{
+          demo:[0,0,0,0,0,0]
+      }
+  },
+  methods:{
+      reduce(index){
+          console.log(index)
+      },
+      addnum(index){
+           console.log(index)
+           let value=this.demo[index]++;
+           Vue.set(this.demo,this.demo[index],value)
+           console.log(this.demo)
+      }
+  }
+}
+</script>
